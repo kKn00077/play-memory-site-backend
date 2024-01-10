3acb4c060c2ede196e89fd1a64ce5cdb9dae8cd4
김규나 <kkn00077@gmail.com>
2024-01-10 11:06:53 +0900

    .env 파일 생성 및 runtime 환경 구성

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env.${process.env.NODE_ENV}`,
+            cache: true,
+            expandVariables: true,
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string()
+                    .valid('dev', 'prod', 'stg', 'local')
+                    .default('prod'),
+                PORT: Joi.number().default(3000),
+                DATABASE_ID: Joi.string().required(),
+                DATABASE_PW: Joi.string().required(),
+                MONGODB_URI: Joi.string().required(),
+            }),
+        }),
+    ],
     controllers: [GatewayController],
     providers: [GatewayService],
 })

2934d88437e5ee324a933da31632b0d03303cb46
김규나 <kkn00077@gmail.com>
2024-03-10 15:01:29 +0900

    테스트코드 수정

--- libs/common/src/redis/redis.service.spec.ts
+++ libs/common/src/redis/redis.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RedisService } from './redis.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('RedisService', () => {
     let service: RedisService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [RedisService],
+            providers: [RedisService, ConfigService],
         }).compile();
 
         service = module.get<RedisService>(RedisService);
